b9516de795a39447935809a74719fa5e2521df4f
d5ng <dong5ffice@gmail.com>
2023-07-06 16:49:42 +0900

    react-router Link

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react"
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import Home from "./pages/Home"
+import Products from "./pages/Products"
 
-const router = createBrowserRouter([{ path: "/", element: <Home /> }])
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<Home />} />
+//     <Route path="/" element={<Products />} />
+//   </Route>
+// )
+
+// const router = createBrowserRouter(routeDefinitions)
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/products", element: <Products /> },
+])
 
 function App() {
   return <RouterProvider router={router} />
